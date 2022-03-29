e711666bbd058ab955d395829a2d902b376a00f8
William Nicholson <nicho268@gmail.com>
2022-03-28 20:17:53 -0600

    add change location functionality to modal

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,22 +1,32 @@
-import Weather from "./weather.js";
-import UI from "./ui.js";
+import Weather from './weather.js'
+import UI from './ui.js'
 
-const weather = new Weather("Miami", "United States");
-const ui = new UI();
-
-const data = weather.getWeather();
-
+const weather = new Weather('Miami', 'United States')
+const ui = new UI()
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather)
 
+// Change location event
+document.querySelector('.modal__btn-submit').addEventListener('click', e => {
+  e.preventDefault()
+
+  const city = document.getElementById('input-cities').value
+  const country = document.getElementById('input-countries').value
+
+  weather.changeLocation(city, country)
+
+  // Get and display weather
+  getWeather()
+})
+
 function getWeather() {
   weather
     .getWeather()
-    .then((results) => ui.render(results))
-    .catch((error) => console.log(error));
+    .then(results => ui.render(results))
+    .catch(error => console.log(error))
 }
 
 // TODO Save results to local storage
-// Finish modal to include labels, update styles, add close button and save button.
-// Error handling - perhaps look into filtering cities based on country?
+// TODO  Finish modal to include labels, update styles, add close button and save button.
+// TODO  Error handling - perhaps look into filtering cities based on country?
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -4,13 +4,7 @@ import { countries } from "./countryData.js";
 const datalistCities = document.getElementById('cities')
 const datalistCountries = document.getElementById('countries')
 
-const locationForm = document.querySelector('[data-location-form]')
-
-const LOCAL_STORAGE_CITY_KEY = 'city.name';
-const LOCAL_STORAGE_COUNTRY_KEY = 'country.name';
-
-
-
+// Populate datalists 
 window.onload = function() {
 	let cityOptions = cities.map(city => `<option value="${city.name}">${city.name}</option>`).join('\n');
   let countryOptions = countries.map(country => `<option value="${country.Name}">${country.Name}</option>`).join('\n')
